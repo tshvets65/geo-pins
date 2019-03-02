626031fe25d6c409da8d54e440b4d23dc407ed27
Tetyana Shvets <tshvets@hotmail.com>
2019-03-01 16:10:21 -0800

    Styling login page

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -3,35 +3,53 @@ import { GraphQLClient } from 'graphql-request'
 import { GoogleLogin } from 'react-google-login'
 
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 import Context from '../../context'
+import { ME_QUERY } from '../../graphql/queries'
 
-const ME_QUERY = `{
-  me {
-    _id
-    name
-    email
-    picture
-  }
-}`
 
 const Login = ({ classes }) => {
   const { dispatch } = useContext(Context)
 
   const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token
-    const client = new GraphQLClient('http://localhost:4000/graphql', {
-      headers: { authorization: idToken }
-    })
-    const data = await client.request(ME_QUERY)
-    dispatch({ type: 'LOGIN_USER', payload: data.me })
+    try {
+      const idToken = googleUser.getAuthResponse().id_token
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: { authorization: idToken }
+      })
+      const { me } = await client.request(ME_QUERY)
+      dispatch({ type: 'LOGIN_USER', payload: me })
+    } catch (err) {
+      onFailure(err)
+    }
   }
 
-  return <GoogleLogin
-    clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
-    onSuccess={onSuccess}
-    isSignedIn={true} />
+  const onFailure = err => {
+    console.error('Error logging in', err)
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component='h1'
+        variant='h3'
+        gutterBottom
+        noWrap
+        style={{ color: 'rgb(66, 133, 244' }}
+      >
+        Welcome
+    </Typography>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        theme='dark'
+      />
+    </div>
+
+  )
 };
 
 const styles = {
